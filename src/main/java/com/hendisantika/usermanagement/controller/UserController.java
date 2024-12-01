b2de79f805a519816dfbb172171d7dd645322fa2
YEMONVM <yemonvm@gmail.com>
2024-12-01 23:00:46 +0100

    updated controller

--- src/main/java/com/hendisantika/usermanagement/controller/UserController.java
+++ src/main/java/com/hendisantika/usermanagement/controller/UserController.java
@@ -213,7 +213,7 @@ public class UserController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-        return ResponseEntity.ok("Success!");
+        return ResponseEntity.ok("Success!!");
     }
 
 }
